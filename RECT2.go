3cf6f7f4e1402886094ba995deb6d6b55840ffbc
Emmanuell sensai <emmanuelusang60@gmail.com>
2025-11-24 11:29:38 +0100

    Implement QuadB function to draw a rectangle

---
+++ RECT2.go
@@ -0,0 +1,55 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func QuadB(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for row := 1; row <= y; row++ {
+		for col := 1; col <= x; col++ {
+			isFirstRow := row == 1
+			isLastRow := row == y
+			isFirstCol := col == 1
+			isLastCol := col == x
+
+			isTopLeft := isFirstRow && isFirstCol
+			isTopRight := isFirstRow && isLastCol
+			isBottomLeft := isLastRow && isFirstCol
+			isBottomRight := isLastRow && isLastCol
+
+			isTopEdge := isFirstRow && col > 1 && col < x
+			isBottomEdge := isLastRow && col > 1 && col < x
+			isLeftEdge := isFirstCol && row > 1 && row < y
+			isRightEdge := isLastCol && row > 1 && row < y
+
+			if isTopLeft {
+				z01.PrintRune('/')
+			} else if isTopRight {
+				z01.PrintRune('\\')
+			} else if isBottomLeft {
+				z01.PrintRune('\\')
+			} else if isBottomRight {
+				z01.PrintRune('/')
+			} else if isTopEdge || isBottomEdge {
+				z01.PrintRune('*')
+			} else if isLeftEdge || isRightEdge {
+				z01.PrintRune('*')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
+
+func main() {
+	QuadB(5, 3)
+	z01.PrintRune('\n')
+	QuadB(5, 1)
+	z01.PrintRune('\n')
+	QuadB(1, 1)
+	z01.PrintRune('\n')
+	QuadB(1, 5)
+}
